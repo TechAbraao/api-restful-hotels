90c420eb3ffaa66f8d1c2b2c3656f20b94240104
Abraão Santos <abraao.santos@sec4you.com.br>
2025-02-14 21:04:28 -0300

    implementação do put

--- resources/hotel.py
+++ resources/hotel.py
@@ -4,18 +4,21 @@ import json
 with open("./data/data.json", 'r') as arquivo:
     json_hoteis = json.load(arquivo)
 
-
 class Hoteis(Resource):
-
     def get(self):
         return json_hoteis, 201
 
 class Hotel(Resource):
-
-    def get(self, hotel_id):
+    def encontrar_hotel(self, hotel_id):
         for hotel in json_hoteis["hoteis"]:
             if hotel["hotel_id"] == hotel_id:
-                return hotel, 200
+                return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = self.encontrar_hotel(hotel_id)
+        if hotel:
+            return hotel
         return { "message" : "Hotel not found."}, 404
 
     def post(self, hotel_id):
@@ -25,12 +28,24 @@ class Hotel(Resource):
         argumentos.add_argument("diaria")
         argumentos.add_argument("cidade")
         dados = argumentos.parse_args()
-        novo_hotel = {
-            "hotel_id" : hotel_id,
-            "nome" : dados["nome"],
-            "estrelas" : dados["estrelas"],
-            "diaria" : dados["diaria"],
-            "cidade" : dados["cidade"]
-        }
+
+        novo_hotel = {"hotel_id" : hotel_id, **dados}
         json_hoteis["hoteis"].append(novo_hotel)
         return novo_hotel, 200
+
+    def put(self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument("nome")
+        argumentos.add_argument("estrelas")
+        argumentos.add_argument("diaria")
+        argumentos.add_argument("cidade")
+        dados = argumentos.parse_args()
+
+        novo_hotel = {"hotel_id" : hotel_id, **dados}
+        hotel = self.encontrar_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        else:
+            json_hoteis["hoteis"].append(novo_hotel)
+            return novo_hotel, 201 # Criando novo hotel com sucesso
